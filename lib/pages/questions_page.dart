d0cb1f121fd69c4693f4b9796e3d5fba4f5f7364
GadianoC <cgadiano22a-0029@olopsc.edu.ph>
2024-05-16 09:37:24 +0800

    olopscform

--- lib/pages/questions_page.dart
+++ lib/pages/questions_page.dart
@@ -1,4 +1,6 @@
+import 'package:alumni_app/compontents/question_form.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class QuestionsPage extends StatefulWidget {
   const QuestionsPage({super.key});
@@ -8,6 +10,35 @@ class QuestionsPage extends StatefulWidget {
 }
 
 class _QuestionsPageState extends State<QuestionsPage> {
+  final List _firstQuestion = [
+    {'value': false, 'answer': 'yes'},
+    {'value': false, 'answer': 'no'},
+  ];
+
+  final List _secondQuestion = [
+    {'value': false, 'answer': 'Not relevant at all'},
+    {'value': false, 'answer': 'Somewhat relevant'},
+    {'value': false, 'answer': 'Very relevant'},
+  ];
+
+  final List _thirdQuestion = [
+    {'value': false, 'answer': 'Not helpful at all'},
+    {'value': false, 'answer': 'Somewhat helpful'},
+    {'value': false, 'answer': 'Very helpful'},
+  ];
+
+  final List _fourthQuestion = [
+    {'value': false, 'answer': 'No'},
+    {'value': false, 'answer': 'Somehow'},
+    {'value': false, 'answer': 'Yes'},
+  ];
+
+  final List _fifthQuestion = [
+    {'value': false, 'answer': 'Not relevant at all'},
+    {'value': false, 'answer': 'Somewhat relevant'},
+    {'value': false, 'answer': 'Same/very relevant'},
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,56 +53,230 @@ class _QuestionsPageState extends State<QuestionsPage> {
           ],
         ),
       ),
-      body: Center(
-        child: Column(
-          children: [
-            const Icon(
-              Icons.abc,
-              size: 80,
-            ),
-            const Text('Our Lady of Perpetual Succor College'),
-            const Text('Alumni Tracking System'),
-            const SizedBox(
-              height: 12,
-            ),
-            const Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text('Made By: '),
-                Icon(Icons.abc),
-              ],
-            ),
-            const SizedBox(
-              height: 75,
-            ),
-            Container(
-              child: Column(
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            children: [
+              const Icon(
+                Icons.abc,
+                size: 80,
+              ),
+              const Text('Our Lady of Perpetual Succor College'),
+              const Text('Alumni Tracking System'),
+              const SizedBox(
+                height: 12,
+              ),
+              const Row(
+                mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text('data'),
-                      Text('data'),
-                    ],
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text('data'),
-                      Text('data'),
-                    ],
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text('data'),
-                      Text('data'),
-                    ],
-                  ),
+                  Text('Made By: '),
+                  Icon(Icons.abc),
                 ],
               ),
-            ),
-          ],
+              const SizedBox(
+                height: 75,
+              ),
+              Container(
+                child: Column(
+                  children: [
+                    //1st Question
+                    QuestionForm(
+                      content: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          const Align(
+                            alignment: Alignment.centerLeft,
+                            child: Text(
+                                'Are you employed within the year of your graduation?'),
+                          ),
+                          Column(
+                              children: List.generate(
+                            _firstQuestion.length,
+                            (index) => CheckboxListTile(
+                              contentPadding: const EdgeInsets.all(4),
+                              checkboxShape: const CircleBorder(),
+                              controlAffinity: ListTileControlAffinity.leading,
+                              dense: true,
+                              title: Text(
+                                _firstQuestion[index]['answer'],
+                                style: const TextStyle(),
+                              ),
+                              value: _firstQuestion[index]['value'],
+                              onChanged: (value) {
+                                setState(() {
+                                  for (var element in _firstQuestion) {
+                                    element['value'] = false;
+                                  }
+                                  _firstQuestion[index]['value'] = value;
+                                });
+                              },
+                            ),
+                          )),
+                        ],
+                      ),
+                    ),
+                    //2nd Question
+                    QuestionForm(
+                      content: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          const Align(
+                            alignment: Alignment.centerLeft,
+                            child: Text(
+                                'How relevant is the program you took to your job after graduation?'),
+                          ),
+                          Column(
+                              children: List.generate(
+                            _secondQuestion.length,
+                            (index) => CheckboxListTile(
+                              contentPadding: const EdgeInsets.all(4),
+                              checkboxShape: const CircleBorder(),
+                              controlAffinity: ListTileControlAffinity.leading,
+                              dense: true,
+                              title: Text(
+                                _secondQuestion[index]['answer'],
+                                style: const TextStyle(),
+                              ),
+                              value: _secondQuestion[index]['value'],
+                              onChanged: (value) {
+                                setState(() {
+                                  for (var element in _secondQuestion) {
+                                    element['value'] = false;
+                                  }
+                                  _secondQuestion[index]['value'] = value;
+                                });
+                              },
+                            ),
+                          )),
+                        ],
+                      ),
+                    ),
+                    //3rd Question
+                    QuestionForm(
+                      content: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          const Align(
+                            alignment: Alignment.centerLeft,
+                            child: Text(
+                                'Does the program you took helped you land your first job?'),
+                          ),
+                          Column(
+                              children: List.generate(
+                            _thirdQuestion.length,
+                            (index) => CheckboxListTile(
+                              contentPadding: const EdgeInsets.all(4),
+                              checkboxShape: const CircleBorder(),
+                              controlAffinity: ListTileControlAffinity.leading,
+                              dense: true,
+                              title: Text(
+                                _thirdQuestion[index]['answer'],
+                                style: const TextStyle(),
+                              ),
+                              value: _thirdQuestion[index]['value'],
+                              onChanged: (value) {
+                                setState(() {
+                                  for (var element in _thirdQuestion) {
+                                    element['value'] = false;
+                                  }
+                                  _thirdQuestion[index]['value'] = value;
+                                });
+                              },
+                            ),
+                          )),
+                        ],
+                      ),
+                    ),
+                    //4th Question
+                    QuestionForm(
+                      content: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          const Align(
+                            alignment: Alignment.centerLeft,
+                            child: Text(
+                                'Were you able to aquire the necessary skills from the program you took that are needed for your current job?'),
+                          ),
+                          Column(
+                              children: List.generate(
+                            _fourthQuestion.length,
+                            (index) => CheckboxListTile(
+                              contentPadding: const EdgeInsets.all(4),
+                              checkboxShape: const CircleBorder(),
+                              controlAffinity: ListTileControlAffinity.leading,
+                              dense: true,
+                              title: Text(
+                                _fourthQuestion[index]['answer'],
+                                style: const TextStyle(),
+                              ),
+                              value: _fourthQuestion[index]['value'],
+                              onChanged: (value) {
+                                setState(() {
+                                  for (var element in _fourthQuestion) {
+                                    element['value'] = false;
+                                  }
+                                  _fourthQuestion[index]['value'] = value;
+                                });
+                              },
+                            ),
+                          )),
+                        ],
+                      ),
+                    ),
+                    //5th Question
+                    QuestionForm(
+                      content: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          const Align(
+                            alignment: Alignment.centerLeft,
+                            child: Text('Is your job related to your course?'),
+                          ),
+                          Column(
+                              children: List.generate(
+                            _fifthQuestion.length,
+                            (index) => CheckboxListTile(
+                              contentPadding: const EdgeInsets.all(4),
+                              checkboxShape: const CircleBorder(),
+                              controlAffinity: ListTileControlAffinity.leading,
+                              dense: true,
+                              title: Text(
+                                _fifthQuestion[index]['answer'],
+                                style: const TextStyle(),
+                              ),
+                              value: _fifthQuestion[index]['value'],
+                              onChanged: (value) {
+                                setState(() {
+                                  for (var element in _fifthQuestion) {
+                                    element['value'] = false;
+                                  }
+                                  _fifthQuestion[index]['value'] = value;
+                                });
+                              },
+                            ),
+                          )),
+                        ],
+                      ),
+                    ),
+                    //6th Question
+                    QuestionForm(
+                      content: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          const Align(
+                            alignment: Alignment.centerLeft,
+                            child: Text(
+                                'What are the skills you aquired from the program you took, that you find helpful in your current job?'),
+                          ),
+                          Text('data'),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
