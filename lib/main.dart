233baef3e25b9f83a533da9eb26daaf01931190a
Anjellyace <angelicakristiannepre1@gmail.com>
2024-05-22 15:47:13 +0800

    added welcome page

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:alumni_app/firebase_options.dart';
 import 'package:alumni_app/pages/home_page.dart';
+import 'package:alumni_app/pages/welcome_page.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:animated_text_kit/animated_text_kit.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -16,7 +18,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomePage(),
+      home: WelcomePage(),
+      // home: HomePage(),
     );
   }
 }
